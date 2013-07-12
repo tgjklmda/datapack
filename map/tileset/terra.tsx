96d630d7b0cd6d4121bc780b34486b391cb8c93f
BRULE <tgjklmda@hotmail.Fr>
2013-07-12 09:25:22 +0200

    map tilset

--- map/tileset/terra.tsx
+++ map/tileset/terra.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="1" tilewidth="16" tileheight="16">
+<tileset name="Terra" tilewidth="16" tileheight="16">
  <image source="terra.png" trans="000000" width="512" height="512"/>
  <tile id="32">
   <properties>
